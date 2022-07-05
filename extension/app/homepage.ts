c1d40b31c4d75c6057ba5f071e386f8164abb2d1
Jean-Christophe Chevalier <jean-christophe.chevalier@ledger.fr>
2022-07-05 12:44:47 +0200

    impl qui marche mieux

--- extension/app/homepage.ts
+++ extension/app/homepage.ts
@@ -1,7 +1,6 @@
 import WalletConnect from "@walletconnect/client";
 import {ITxData} from "@walletconnect/types"
 import { AccountInfos } from "./AccountInfos";
-import { getGasGarometer } from "./getGasBarometer";
 
 export async function loadHomePage(connector: WalletConnect, parameters: AccountInfos) {
     const ethereumButton = document.querySelector('.enableEthereumButton');
@@ -10,7 +9,7 @@ export async function loadHomePage(connector: WalletConnect, parameters: Account
 
     let transaction: ITxData = {
         from: parameters.accounts[0],
-        to: "0xf817d97da72b4f515d74f2dd2e2235b9c22aabf4",
+        to: "0x2b857c8944fbba4d0a63f32bcb6daeb510289adb",
         value: "0x38d7ea4c68000",
         data: "0xe6985b3c0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000b6578616d706c652e636f6d000000000000000000000000000000000000000000",
     };
